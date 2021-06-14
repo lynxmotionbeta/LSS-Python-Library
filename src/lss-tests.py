5cfaf8df88d14bbcad158352ff6528469b601379
Colin MacKenzie <colin@flyingeinstein.com>
2021-06-14 11:57:53 -0400

    first action tests

--- src/lss-tests.py
+++ src/lss-tests.py
@@ -40,6 +40,43 @@ class LssQueryTests(unittest.TestCase):
             self.assertEqual(p.command, 'S')
 
 
+
+class LssActionTests(unittest.TestCase):
+
+    def assertBetween(self, v: int, min: int, max: int):
+        self.assertGreaterEqual(v, min)
+        self.assertLessEqual(v, max)
+
+    def assertServoQueryWithin(self, servo: int, parameter: str, value: int, precision: int = 5):
+        bus.write_command(servo, f'Q{parameter}')
+        p = bus.read()
+        self.assertIsNotNone(p)
+        self.assertTrue(p.known)
+        self.assertEqual(p.command, 'D')
+        self.assertBetween(p.value, value - precision, value + precision)
+
+    def test_D(self):
+        for servo in servos:
+            bus.write_command(servo, 'D1500')
+            time.sleep(1.5)
+            self.assertServoQueryWithin(servo, 'D', 1500, 10)
+            bus.write_command(servo, 'D500')
+            time.sleep(0.8)
+            self.assertServoQueryWithin(servo, 'D', 500, 10)
+            bus.write_command(servo, 'D0')
+            time.sleep(0.8)
+            self.assertServoQueryWithin(servo, 'D', 0, 10)
+            bus.write_command(servo, 'D-500')
+            time.sleep(0.8)
+            self.assertServoQueryWithin(servo, 'D', -500, 10)
+            bus.write_command(servo, 'D-1500')
+            time.sleep(0.8)
+            self.assertServoQueryWithin(servo, 'D', -1500, 10)
+            bus.write_command(servo, 'D0')
+            time.sleep(1.2)
+            self.assertServoQueryWithin(servo, 'D', 0, 10)
+
+
 if __name__ == '__main__':
     unittest.main()
     bus.close()
